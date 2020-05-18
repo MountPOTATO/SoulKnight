b119a76c47dc4746f25543ffd747396e8af0feb8
NathanHolmes4 <503253721@qq.com>
2020-05-19 00:28:51 +0800

    修改HelloWorld实现并设置HRocker

--- SoulKnight2020/Classes/HelloWorldScene.cpp
+++ SoulKnight2020/Classes/HelloWorldScene.cpp
@@ -52,69 +52,67 @@ bool HelloWorld::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+	initHRocker();
 
-    if (closeItem == nullptr ||
-        closeItem->getContentSize().width <= 0 ||
-        closeItem->getContentSize().height <= 0)
-    {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-    }
-    else
-    {
-        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
-        float y = origin.y + closeItem->getContentSize().height/2;
-        closeItem->setPosition(Vec2(x,y));
-    }
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+	//5.18 原有的HelloWorld原初始界面，注释待删除
 
-    /////////////////////////////
-    // 3. add your codes below...
+    ///////////////////////////////
+    //// 2. add a menu item with "X" image, which is clicked to quit the program
+    ////    you may modify it.
+    //// add a "close" icon to exit the progress. it's an autorelease object
+    //auto closeItem = MenuItemImage::create(
+    //                                       "CloseNormal.png",
+    //                                       "CloseSelected.png",
+    //                                       CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    //if (closeItem == nullptr ||
+    //    closeItem->getContentSize().width <= 0 ||
+    //    closeItem->getContentSize().height <= 0)
+    //{
+    //    problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
+    //}
+    //else
+    //{
+    //    float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
+    //    float y = origin.y + closeItem->getContentSize().height/2;
+    //    closeItem->setPosition(Vec2(x,y));
+    //}
+    //// create menu, it's an autorelease object
+    //auto menu = Menu::create(closeItem, NULL);
+    //menu->setPosition(Vec2::ZERO);
+    //this->addChild(menu, 1);
+    ///////////////////////////////
+    //// 3. add your codes below...
+    //// add a label shows "Hello World"
+    //// create and initialize a label
+    //auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    //if (label == nullptr)
+    //{
+    //    problemLoading("'fonts/Marker Felt.ttf'");
+    //}
+    //else
+    //{
+    //    // position the label on the center of the screen
+    //    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+    //                            origin.y + visibleSize.height - label->getContentSize().height));
+    //    // add the label as a child to this layer
+    //    this->addChild(label, 1);
+    //}
+    //// add "HelloWorld" splash screen"
+    //auto sprite = Sprite::create("HelloWorld.png");
+    //if (sprite == nullptr)
+    //{
+    //    problemLoading("'HelloWorld.png'");
+    //}
+    //else
+    //{
+    //    // position the sprite on the center of the screen
+    //    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    //    // add the sprite as a child to this layer
+    //    this->addChild(sprite, 0);
+    //}
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
     return true;
 }
 
@@ -129,5 +127,14 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
     //EventCustom customEndEvent("game_scene_close_event");
     //_eventDispatcher->dispatchEvent(&customEndEvent);
 
+
+}
+
+
+void HelloWorld::initHRocker()
+{
+	_rocker = HRocker::createHRocker("rockerBG.png", "rocker.png", Vec2(200, 100));
+	this->addChild(_rocker);
+	_rocker->startRocker(true);
 
 }
