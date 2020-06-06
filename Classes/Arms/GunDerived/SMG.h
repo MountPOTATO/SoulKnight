fd5abdb7992741f3c095e01219168de284394472
NathanHolmes4 <503253721@qq.com>
2020-06-06 21:41:22 +0800

    以武器实例SMG为例设置碰撞遮蔽，成功后即将放入基类实现

--- Classes/Arms/GunDerived/SMG.h
+++ Classes/Arms/GunDerived/SMG.h
@@ -7,4 +7,35 @@ public:
 
 	virtual bool init(const char* weaponImageName1, const char* weaponImageName2,
 		HelloWorld* currentScene, ESide side, bool _heroOwned);
+
+
+
+	void setOwner(Entity* owner) { _owner = owner; }
+
+	void updateCurrentLocation();
+
+public:
+	void setTiledMap(TMXTiledMap* map) {
+		m_map = map;
+		this->meta = m_map->getLayer("Meta");
+		this->meta->setVisible(false);
+	}
+	Point tileCoordForPosition(Point pos) {
+		Size mapTiledNum = m_map->getMapSize();
+		Size tiledSize = m_map->getTileSize();
+		int x = pos.x / tiledSize.width;
+		int y = (mapTiledNum.height * tiledSize.height - pos.y) / tiledSize.height;
+		return Point(x, y);
+	}
+	void setWeaponTiledMap(TMXTiledMap* map) {
+		m_map = map;
+		this->meta = m_map->getLayer("Meta");
+		this->meta->setVisible(false);
+	}
+
+
+
+private:
+	TMXTiledMap* m_map;
+	TMXLayer* meta;
 };
